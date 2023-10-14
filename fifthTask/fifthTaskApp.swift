3b419ff2969d77ddadba2127fa3ff74a54b89d45
TURKCELL/Pavel Nepogodin <pavel.nepogodin@consultant.turcell.com.tr>
2023-10-15 00:17:02 +0300

    Initial Commit

---
+++ fifthTask/fifthTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  fifthTaskApp.swift
+//  fifthTask
+//
+//  Created by  Pavel Nepogodin on 15.10.23.
+//
+
+import SwiftUI
+
+@main
+struct fifthTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
